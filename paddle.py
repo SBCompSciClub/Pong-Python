38effcc8aa9c077fec2878e2ba27a5d41229dffa
Sethu Senthil <sethunarayansenthil@gmail.com>
2019-09-24 21:57:13 -0400

    :sparkles: added requirements.txt & gitignore and removed whitespacing

--- paddle.py
+++ paddle.py
@@ -1,31 +1,31 @@
 import pygame
-BLACK = (0,0,0) 
+BLACK = (0,0,0)
 
 class Paddle(pygame.sprite.Sprite):
     #Paddle Class
-    
+
     def __init__(self, color, width, height):
-        
+
         super().__init__()
-        
+
         #initialize image
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
- 
+
         # Draw the rectangle
         pygame.draw.rect(self.image, color, [0, 0, width, height])
-        
-        #get rectangle 
+
+        #get rectangle
         self.rect = self.image.get_rect()
-        
+
     def moveUp(self, pixels):
     	#move up
         self.rect.y -= pixels
-        #Check if too far up 
+        #Check if too far up
         if self.rect.y < 0:
           self.rect.y = 0
-          
+
     def moveDown(self, pixels):
     	#move down
         self.rect.y += pixels
